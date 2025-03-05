596a774d680a8bae973c779a0687c5e83a4d1239
yumachin <curryuma925@gmail.com>
2025-03-05 20:42:23 +0900

    create Next.js starter template

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
+import { FlatCompat } from "@eslint/eslintrc";
+// npm i --save-dev eslint-plugin-import
+import importPlugin from "eslint-plugin-import";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +14,33 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // 以下を記述
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          // ➀ npm して入れるやつ
+          // ➁ @ で inport するやつ
+          // ➀ 内部で定義したやつ（親 → 兄弟 → 自分）
+          "groups": [
+            ["builtin", "external"],
+            ["internal"],
+            ["parent", "sibling", "index"]
+          ],
+
+          // アルファベット順（大文字と小文字の区別なし）
+          "alphabetize": { "order": "asc", "caseInsensitive": true },
+
+          //　グループごとに改行入れる
+          "newlines-between": "always"
+        }
+      ]
+    }
+  }
 ];
 
 export default eslintConfig;
